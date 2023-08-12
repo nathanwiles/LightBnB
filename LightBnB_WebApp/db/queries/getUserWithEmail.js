943ae09ece8785c89819d02dfe47e3f191fb31b2
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-12 16:04:33 -0400

    modularize getUserWithEmail

---
+++ LightBnB_WebApp/db/queries/getUserWithEmail.js
@@ -0,0 +1,17 @@
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function (email) {
+  const {query} = require('../index')
+  return query(`SELECT * FROM users WHERE email = $1;`, [email])
+    .then((res) => {
+      return res.rows[0] || null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { getUserWithEmail };
